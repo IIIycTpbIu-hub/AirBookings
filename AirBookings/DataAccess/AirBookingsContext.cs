bf2d5880e3de9ab5ab220aa8a7f141ea15480cf4
Andrew Raznopolov <razandrey95@gmail.com>
2020-11-12 10:53:59 +0400

    Add context

---
+++ AirBookings/DataAccess/AirBookingsContext.cs
@@ -0,0 +1,15 @@
+﻿using AirBooking.Models;
+using System.Data.Entity;
+
+namespace AirBookings.DataAccess
+{
+    public class AirBookingsContext : DbContext
+    {
+        public DbSet<Aircraft> Aircrafts {get; set;}
+        public DbSet<Airport> Airports { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Flight> Flights { get; set; }
+        public DbSet<Seat> Seats { get; set; }
+
+    }
+}
