0fed44b9494fd22959d489c19cde3ccffdba2782
Andrew Raznopolov <razandrey95@gmail.com>
2020-11-20 16:25:13 +0400

    Lab3_7

---
+++ AirBookings/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AirBookings.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public decimal Cost { get; set; }
+        public int? EmployerId { get; set; }
+        public Employee Employee { get; set; }
+    }
+}
