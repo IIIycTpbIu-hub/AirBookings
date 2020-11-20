f7dd227cb94dd034f03a1228dbe4588e419363d2
Andrew Raznopolov <razandrey95@gmail.com>
2020-11-20 16:30:36 +0400

    Lab3_8

---
+++ AirBookings/DataAccess/Lab3Context.cs
@@ -0,0 +1,15 @@
+﻿using AirBookings.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AirBookings.DataAccess
+{
+    public class Lab3Context : DbContext 
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
