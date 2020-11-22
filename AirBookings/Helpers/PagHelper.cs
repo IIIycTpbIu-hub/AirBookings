41c1e8c52d720b3d8d00724a950b264c6fc24020
Andrew Raznopolov <razandrey95@gmail.com>
2020-11-22 12:26:08 +0400

    lab4_P_4

---
+++ AirBookings/Helpers/PagHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AirBookings.Models;
+using System.Text;
+using AirBookings.Models.Pageing;
+
+namespace AirBookings.Helpers
+{
+    public static class PagHelper
+    {
+        public static MvcHtmlString PageLinks(this HtmlHelper htmlHelper, PagePag pagePag, Func<int, string> pageUrl)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 1; i < pagePag.TotalPages; i++)
+            {
+                TagBuilder tag = new TagBuilder("a");
+                tag.MergeAttribute("href", pageUrl(i));
+                if(i == pagePag.PageNumber)
+                {
+                    tag.AddCssClass("selected");
+                    tag.AddCssClass("btn-primary");
+                }
+                tag.AddCssClass("btn btn-default");
+                sb.Append(tag.ToString());
+            }
+            return MvcHtmlString.Create(sb.ToString());
+        }
+    }
+}
