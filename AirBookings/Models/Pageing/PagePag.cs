87bc08bd2715727f4d1b0da8a933fc91aeae1d11
Andrew Raznopolov <razandrey95@gmail.com>
2020-11-22 11:29:54 +0400

    Lab4_P_2

---
+++ AirBookings/Models/Pageing/PagePag.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AirBookings.Models.Pageing
+{
+    public class PagePag
+    {
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public int TotalProducts { get; set; }
+        public int TotalPages {
+            get { return (int)Math.Ceiling((double)TotalProducts / PageSize); }
+        } 
+    }
+}
---
+++ AirBookings/Models/Pageing/ViewProducts.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AirBookings.Models.Pageing
+{
+    public class ViewProducts
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public PagePag PagePag { get; set; }
+    }
+}
