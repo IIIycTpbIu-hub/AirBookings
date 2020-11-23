87968d84b899a47f43cae72841b14890bbdc7448
Andrew Raznopolov <razandrey95@gmail.com>
2020-11-23 10:36:17 +0400

    Lab4_V_7-9

--- AirBookings/Models/Product.cs
+++ AirBookings/Models/Product.cs
@@ -14,9 +14,12 @@ namespace AirBookings.Models
         public int Id { get; set; }
         [Display(Name = "Название услуги")]
         [Required(ErrorMessage ="Вам необходимо заполнить поле Название услуги")]
+        [RegularExpression(@"[^0-9]", ErrorMessage = "любые симовлы кроме цифр")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Длина строки должна быть от 2 до 50 символов")]
         public string ProductName { get; set; }
         [Display(Name = "Стоимость")]
         [Required]
+        [Range(0, 10000, ErrorMessage = "слишком большая стоимость услуги")]
         public decimal Cost { get; set; }
         [Column("Employee_Id")]
         [Display(Name = "Исполнитель")]
